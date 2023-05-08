ff6a859065d1be7b5fa1687d37c842e14d521845
Victor Moraes de Aragão <vct.aragao@gmail.com>
2023-05-08 14:08:50 -0300

    feat: add descriptions to populate template

--- internal/entity/content.go
+++ internal/entity/content.go
@@ -1,6 +1,7 @@
 package entity
 
 type Content struct {
-	Name string `json:"name"`
-	Code string `json:"code"`
+	Name         string   `json:"name"`
+	Code         string   `json:"code"`
+	Descriptions []string `json:"descriptions"`
 }
